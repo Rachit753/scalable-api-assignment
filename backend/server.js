0931367b686551520225cf79054b7a4fa1dc6d72
Rachit753 <chauhanrachit02@gmail.com>
2026-03-14 12:15:17 +0530

    Add MongoDB Atlas connection and environment configuration

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
 
 const app = express();
+
+connectDB();
 
 app.use(express.json());
 
@@ -8,7 +14,7 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
