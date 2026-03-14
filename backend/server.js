6041128e28677208c68e99e01566e614b4834849
Rachit753 <chauhanrachit02@gmail.com>
2026-03-14 13:01:45 +0530

    Add JWT authentication middleware for protected routes and role-based access control with admin route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 
 const authRoutes = require("./routes/authRoutes");
+const protect = require("./middleware/authMiddleware");
+const authorizeRoles = require("./middleware/roleMiddleware");
 
 dotenv.config();
 
@@ -16,6 +18,20 @@ app.use("/api/v1/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
+});
+
+app.get("/api/v1/protected", protect, (req, res) => {
+  res.json({
+    message: "Protected route accessed",
+    user: req.user
+  });
+});
+
+app.get("/api/v1/admin", protect, authorizeRoles("admin"), (req, res) => {
+  res.json({
+    message: "Welcome Admin",
+    user: req.user
+  });
 });
 
 const PORT = process.env.PORT || 5000;
