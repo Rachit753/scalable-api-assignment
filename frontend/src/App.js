a0a2175c85cece3ee4085456aec4df4c9f92e88c
Rachit753 <chauhanrachit02@gmail.com>
2026-03-14 16:04:53 +0530

    Initialize React frontend for API testing UI

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Scalable API Assignment</h1>
+      <p>Frontend is running successfully.</p>
+    </div>
+  );
+}
+
+export default App;
